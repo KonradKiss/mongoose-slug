cbf1d66b9bcb47fd2f6b04320855e7fd33f1feb1
Konrad Kiss <konrad@konradkiss.com>
2015-09-05 14:30:44 +0200

    Added unique slug support per the linkmesrl PR

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+'use strict';
 /**
  * deps
  */
@@ -17,14 +17,18 @@ var slug = require('speakingurl');
  *      - `.replace` characters to replace defaulted to `[^a-zA-Z]`
  *      - `.separator` separator to use, defaulted to `-`
  *      - `required` whether a slug is required, defaults to `true`
+ *      - `unique` whether the slug should be unique, defaults to `false`
  *
  * @param {String} prop
  * @param {Object} options
  * @return {Function}
  */
 
-module.exports = function(prop, opts){
-  return (function slugize(schema){
+module.exports = function(prop, opts) {
+  return function slugize(schema) {
+
+    var unique = (opts && opts.unique) ? true : false;
+
     var title;
     schema.add({
       slug: {
@@ -34,15 +38,17 @@ module.exports = function(prop, opts){
     });
 
     if (opts && opts.track) {
-      schema.add({ slugs: [ String ] });
+      schema.add({
+        slugs: [String]
+      });
     }
 
-    schema.pre('save', function(next){
+    schema.pre('save', function(next) {
       var self = this;
 
       if (prop && Array.isArray(prop)) {
         var titles = [];
-        prop.forEach(function(el){
+        prop.forEach(function(el) {
           titles.push(self[el]);
         });
         title = titles.join(' ');
@@ -50,16 +56,41 @@ module.exports = function(prop, opts){
         title = this[prop || 'title'];
       }
 
-      var require = (opts && opts.required === false) ? false : true
-        , presets = (opts && opts.override) ? true : false;
+      var require = (opts && opts.required === false) ? false : true;
 
-      if (require && !title) return next(new Error(prop + ' is required to create a slug'));
+      if (require && !title) {
+        return next(new Error(prop + ' is required to create a slug'));
+      }
 
       var mySlug = slug(title, opts);
-      if (opts && opts.track && self.slugs && self.slugs.indexOf( mySlug) == -1) self.slugs.push( mySlug );
-      if (title && !self.slug) self.slug = mySlug;
+      var isChanged = self.isNew || (self.slug !== mySlug);
 
-      next();
+      if (!isChanged) {
+        return next();
+      }
+
+      if (opts && opts.track && self.slugs && self.slugs.indexOf(mySlug) === -1) {
+        self.slugs.push(mySlug);
+      }
+
+      if (title && !self.slug) {
+        self.slug = mySlug;
+      }
+
+      if (unique) {
+
+        this.constructor.count({
+          slug: self.slug
+        }, function(err, number) {
+          if (number >= 1 && self.isNew) {
+            self.slug = self.slug + '-' + number;
+          }
+          next();
+        });
+      } else {
+        next();
+      }
+
     });
-  });
+  };
 };
